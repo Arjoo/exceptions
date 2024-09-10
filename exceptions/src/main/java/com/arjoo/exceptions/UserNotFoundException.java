777060e4bd837e6e3fe58f9399ec251424e5ab66
arjoo <2009arjoo@gmail.com>
2024-09-10 23:48:55 +0530

    Added changes

--- exceptions/src/main/java/com/arjoo/exceptions/UserNotFoundException.java
+++ exceptions/src/main/java/com/arjoo/exceptions/UserNotFoundException.java
@@ -1,5 +1,7 @@
 package com.arjoo.exceptions;
 
+import java.util.function.Supplier;
+
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String msg) {
         super(msg);
--- exceptions/src/main/java/com/arjoo/exceptions/UserServiceImpl.java
+++ exceptions/src/main/java/com/arjoo/exceptions/UserServiceImpl.java
@@ -2,14 +2,18 @@ package com.arjoo.exceptions;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Service
 public class UserServiceImpl implements UserService {
     @Override
     public User findUserById(Integer id) {
-        if(id == 100) {
-            return new User(100, "Arjoo");
-        }else {
-            throw new UserNotFoundException("User not found!!");
-        }
+        List<User> users = Arrays.asList(new User(100, "Arjoo"));
+        User user = users.stream()
+                .filter(u -> u.id().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new UserNotFoundException("Hi.. User not found"));
+        return user;
     }
 }
